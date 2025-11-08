376d6e9de042659b0c405a729233c22593f52e40
andreabrantes80 <meloabrantes@gmail.com>
2025-11-08 11:26:12 -0300

    Função que calcula o taamnho do texto.

---
+++ src/Funcoes/Tamanho_Texto.java
@@ -0,0 +1,17 @@
+package Funcoes;
+
+public class Tamanho_Texto {
+
+    public static void main(String[] args) {
+
+        System.out.println("Tamanho do texto: "+tamamhoTexto("São Paulo"));
+
+
+
+    }
+
+    public static int tamamhoTexto(String nome){
+        return nome.length();
+
+    }
+}
