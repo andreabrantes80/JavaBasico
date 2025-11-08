55bb99752b28ad49cc3562af230c0a47eb56c7a6
andreabrantes80 <meloabrantes@gmail.com>
2025-11-08 11:18:26 -0300

    Função que calcula a média de dois números.

---
+++ src/Funcoes/Calcula_Media.java
@@ -0,0 +1,16 @@
+package Funcoes;
+
+public class Calcula_Media {
+
+    public static void main(String[] args) {
+
+        System.out.println("Média de dois elementos é: " + media(8.0, 7.5));
+
+    }
+
+    public static double media(double a, double b) {
+        return (a + b) / 2;
+    }
+
+
+}
